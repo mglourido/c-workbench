cc3ff1fd4e6ebf70fdb9cc650c3e55175e7e5d70
Mateo González <mlourido1467@gmail.com>
2026-07-25 20:29:01 +0200

    Remove Duplicates In-Place done

---
+++ Two pointers/Remove Duplicates In-Place/solution.c
@@ -0,0 +1,28 @@
+int removeDuplicates(int *nums, int sizeNums) {
+  int k = sizeNums;
+
+  int pointerSet = 0; // pointer that sets the number being checked
+  int pointerSearch = 0;
+
+  while (pointerSet < k) {
+    const int numRevise = nums[pointerSet];
+    pointerSearch = pointerSet;
+
+    while (nums[pointerSearch] == numRevise)
+      pointerSearch++;
+
+    int steps = pointerSearch - pointerSet;
+    k -= steps;
+
+    // reposition elements
+    while (pointerSearch < k) {
+      steps++;
+      pointerSearch++;
+      nums[pointerSearch - steps] = nums[pointerSearch];
+    }
+
+    pointerSet++;
+  }
+
+  return k;
+}
